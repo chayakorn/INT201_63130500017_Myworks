a27dd95b25c2df8466a5c15f1509f2ed8fd3b746
chayakorn <72536627+chayakorn@users.noreply.github.com>
2021-08-24 13:39:00 +0700

    bmical&zodiac edit

---
+++ work/BMIcalculator.js
@@ -0,0 +1,4 @@
+const bmiCalbypounds_inches = (weight,height)=>{
+    return (weight*0.453592)/Math.pow((height*0.0254),2);
+}
+console.log(bmiCalbypounds_inches(36.5,39));
--- work/zodiac.js
+++ work/zodiac.js
@@ -1,19 +1,7 @@
-function zodiac(a) {
-    let date = new Date;
-    a = date.getFullYear() < a?a - 543:a;
-    switch(a%12){
-        case(0): return 'monkey';
-        case(1): return 'rooster';
-        case(2): return 'dog';
-        case(3): return 'pig';
-        case(4): return 'rat';
-        case(5): return 'ox';
-        case(6): return 'tiger';
-        case(7): return 'rabbit';
-        case(8): return 'dragon';
-        case(9): return 'snake';
-        case(10): return 'horses';
-        case(11): return 'sheep';
-    }
+function zodiac(year) {
+    let zodiac = ['monkey','rooster','dog','pig','rat','ox','tiger','rabbit','dragon','snake','horses','sheep'];
+    year = new Date().getFullYear() < year?year - 543:year;
+    return zodiac[year%12];
 }
+console.log(zodiac(2001));
 console.log(zodiac(2544));
